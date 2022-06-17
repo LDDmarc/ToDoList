b3009bc1855d1314d02d9219b922f87a0f0a063e
Daria <daria@fitingo.com>
2022-06-17 19:32:37 +0200

    add listView

--- ToDoList/ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
+let dataManager: DataManager = UserDefaultsDataManager()
+let contentProvider: ContentProvider = ToDoListContentProvider(dataManager: dataManager)
+//let contentProvider: ContentProvider = MockContentProvider()
+
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(contentProvider: contentProvider)
         }
     }
 }
--- ToDoList/ToDoList/View/ContentView.swift
+++ ToDoList/ToDoList/View/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let contentProvider: ContentProvider
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        NavigationView {
+            TaskListView(viewModel: TaskListViewModel(contentProvider: contentProvider))
+        }
     }
 }
