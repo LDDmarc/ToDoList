ff3e8a0feddccbfba8b37ccc958b0dde6bd88da1
Daria <daria@fitingo.com>
2022-06-17 18:14:32 +0200

    init project

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Дарья Леонова on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
